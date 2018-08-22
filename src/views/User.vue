440400ef8e2c4c252df964ab0d3b7d0d3f48353f
austintackaberry <austintackaberry@gmail.com>
2018-08-22 02:29:00 -0700

    feat(User): Allow user to add entries

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,8 +2,7 @@
   <div class="about">
     <h1>This is {{$route.params.username}}'s page</h1>
     <h2>{{date}}</h2>
-    <h3>Entries</h3>
-    <form>
+    <form @submit.prevent="onSubmit">
       <p>Add an entry:</p>
       <p>
         <label for="entryType">Type of entry:</label>
@@ -12,19 +11,32 @@
           <option>Feeling</option>
         </select>
       </p>
-    <p v-if="entryType === 'Consumption'">
-      <label for="foodsConsumed">Foods consumed:</label>
-      <input id="foodsConsumed" v-model="foodsConsumed" placeholder="cappuccino, salad, etc.">
-    </p>
-    <p v-if="entryType === 'Feeling'">
-      <label for="feeling">Feeling:</label>
-      <input id="feeling" v-model="feeling" placeholder="upset stomach, etc.">
-    </p>
-    <p v-if="entryType">
-      <label for="timestamp">{{entryType === 'Feeling' ? "Time felt:" : "Time consumed:"}}</label>
-      <input id="timestamp" v-model="timestamp" placeholder="8:00 AM">
-    </p>
+      <div v-if="entryType">
+        <p v-if="entryType === 'Consumption'">
+          <label for="foodsConsumed">Foods consumed:</label>
+          <input id="foodsConsumed" v-model="foodsConsumed" placeholder="cappuccino, salad, etc.">
+        </p>
+        <p v-if="entryType === 'Feeling'">
+          <label for="feeling">Feeling:</label>
+          <input id="feeling" v-model="feeling" placeholder="upset stomach, etc.">
+        </p>
+        <p>
+          <label for="timestamp">{{entryType === 'Feeling' ? "Time felt:" : "Time consumed:"}}</label>
+          <input id="timestamp" v-model="timestamp" placeholder="8:00 AM">
+        </p>
+        <p>
+          <input type="submit" value="Submit">  
+        </p>
+      </div>
     </form>
+    <h3>Entries</h3>
+    <div class="entries" v-for="entry in entries" :key="entry.id">
+        <div class="entry">
+          <p class="entryType">{{entry.entryType}}</p>
+          <p>{{entry.foodsConsumed ? "Time consumed: " : "Time felt: "}}<span>{{entry.textTime}}</span></p>
+          <p>{{entry.foodsConsumed || entry.feeling }}</p>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -37,10 +49,48 @@ export default {
   data() {
     return {
       date: moment().format("dddd, MMMM Do, YYYY"),
+      entries: [],
       entryType: null,
       foodsConsumed: null,
-      timestamp: null
+      feeling: null,
+      timestamp: moment().format("h:mm A")
     };
+  },
+  methods: {
+    onSubmit() {
+      this.entries.push({
+        entryType: this.entryType,
+        foodsConsumed: this.foodsConsumed,
+        feeling: this.feeling,
+        timestamp: moment(this.timestamp, "h:mm A"),
+        textTime: this.timestamp,
+        id: Math.floor(Math.random() * 1000000)
+      });
+      this.entries.sort((a, b) => a.timestamp - b.timestamp);
+      this.entryType = null;
+      this.feeling = null;
+      this.foodsConsumed = null;
+      this.timestamp = moment().format("h:mm A");
+    }
   }
 };
 </script>
+
+<style scoped>
+.entries {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.entry {
+  border-radius: 3px;
+  border: solid black 1px;
+  padding: 10px;
+  margin: 5px;
+}
+.entryType {
+  font-weight: bold;
+}
+</style>
