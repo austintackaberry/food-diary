a01a7d170995c24a35df62f7fbf872b8dcfd2a10
austintackaberry <austintackaberry@gmail.com>
2018-08-24 03:34:36 -0700

    feat: Get/Add users and entries to firestore

--- src/views/User.vue
+++ src/views/User.vue
@@ -7,16 +7,16 @@
       <p>
         <label for="entryType">Type of entry:</label>
         <select id="entryType" v-model.number="entryType">
-          <option>Consumption</option>
-          <option>Feeling</option>
+          <option>consumption</option>
+          <option>feeling</option>
         </select>
       </p>
       <div v-if="entryType">
-        <p v-if="entryType === 'Consumption'">
-          <label for="foodsConsumed">Foods consumed:</label>
-          <input id="foodsConsumed" v-model="foodsConsumed" placeholder="cappuccino, salad, etc.">
+        <p v-if="entryType === 'consumption'">
+          <label for="consumption">Foods consumed:</label>
+          <input id="consumption" v-model="consumption" placeholder="cappuccino, salad, etc.">
         </p>
-        <p v-if="entryType === 'Feeling'">
+        <p v-if="entryType === 'feeling'">
           <label for="feeling">Feeling:</label>
           <input id="feeling" v-model="feeling" placeholder="upset stomach, etc.">
         </p>
@@ -33,8 +33,8 @@
     <div class="entries" v-for="entry in entries" :key="entry.id">
         <div class="entry">
           <p class="entryType">{{entry.entryType}}</p>
-          <p>{{entry.foodsConsumed ? "Time consumed: " : "Time felt: "}}<span>{{entry.textTime}}</span></p>
-          <p>{{entry.foodsConsumed || entry.feeling }}</p>
+          <p>{{entry.consumption ? "Time consumed: " : "Time felt: "}}<span>{{entry.textTime}}</span></p>
+          <p>{{entry.consumption || entry.feeling }}</p>
         </div>
     </div>
   </div>
@@ -53,7 +53,7 @@ export default {
       ),
       entries: [],
       entryType: null,
-      foodsConsumed: null,
+      consumption: null,
       feeling: null,
       timestamp: moment().format("h:mm A"),
       dataToken: null
@@ -65,23 +65,60 @@ export default {
   },
   methods: {
     onSubmit() {
-      this.entries.push({
+      const updateState = ref => {
+        entry.id = ref.id;
+        this.entries.push({ ...entry, textTime: this.timestamp });
+        this.entries.sort((a, b) => a.timestamp - b.timestamp);
+        this.entryType = null;
+        this.feeling = null;
+        this.consumption = null;
+        this.timestamp = moment().format("h:mm A");
+      };
+
+      const entry = {
         entryType: this.entryType,
-        foodsConsumed: this.foodsConsumed,
-        feeling: this.feeling,
-        timestamp: moment(this.timestamp, "h:mm A"),
-        textTime: this.timestamp,
-        id: Math.floor(Math.random() * 1000000)
-      });
-      this.entries.sort((a, b) => a.timestamp - b.timestamp);
-      this.entryType = null;
-      this.feeling = null;
-      this.foodsConsumed = null;
-      this.timestamp = moment().format("h:mm A");
+        timestamp: moment(this.timestamp, "h:mm A").valueOf(),
+        date: this.$route.params.date,
+        username: this.user.username
+      };
+      entry[this.entryType] = this[this.entryType];
+
+      var firebase = this.$store.state.firebase;
+      var db = firebase.firestore();
+      db.collection("entries")
+        .add(entry)
+        .then(updateState);
     }
   },
-  async mounted() {
-    console.log(this.user);
+  async created() {
+    var firebase = this.$store.state.firebase;
+    var db = firebase.firestore();
+    const newUser = {
+      displayName: this.user.displayName,
+      email: this.user.email,
+      username: this.user.username,
+      phoneNumber: this.user.phoneNumber
+    };
+    await db
+      .collection("users")
+      .doc(this.user.username)
+      .set(newUser, { merge: true });
+    const querySnapshot = await db
+      .collection("entries")
+      .where("username", "==", newUser.username)
+      .where("date", "==", this.$route.params.date)
+      .get();
+    const entries = [];
+    querySnapshot.forEach(doc => {
+      entries.push(doc.data());
+    });
+    this.entries = entries
+      .map(entry => ({
+        ...entry,
+        textTime: moment(entry.timestamp).format("h:mm A")
+      }))
+      .sort((a, b) => a.timestamp - b.timestamp);
+
     if (!this.token) {
       this.dataToken = await this.user.getIdToken();
     } else {
